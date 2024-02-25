bb1d2e431ac1c9df0f9ba8b0885d798e71348567
kelvinnpereira <kelvinn.nunes.p@gmail.com>
2024-02-25 18:46:16 -0400

    rota de api para consultar token de autenticação de usuario

--- GdsSystem/urls.py
+++ GdsSystem/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import UserCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', obtain_auth_token, name='token'),
 ]
