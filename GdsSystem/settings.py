366164dd045db7b4fad1903ce5b8f9650184f656
kelvinnpereira <kelvinn.nunes.p@gmail.com>
2024-03-26 23:50:20 -0400

    usando python-dotenv para carregar variaveis de ambiente

--- GdsSystem/settings.py
+++ GdsSystem/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'GdsSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gds-system',
-        'USER': 'postgres',
-        'PASSWORD': 'senha123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
