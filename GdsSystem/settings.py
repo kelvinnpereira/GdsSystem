b3c837edca425f144b88cf662d5a7fd3e734b18c
kelvinnpereira <kelvinn.nunes.p@gmail.com>
2024-06-09 22:42:12 -0400

    configurando arquivos estaticos

--- GdsSystem/settings.py
+++ GdsSystem/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py migrate
+python3.12 manage.py collectstatic
